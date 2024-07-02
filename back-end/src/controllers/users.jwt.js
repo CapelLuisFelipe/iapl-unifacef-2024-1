3d7b9dc453d4844e8a495d07c00cd376a93d6df0
CapelLuisFelipe <capelluis72@gmail.com>
2024-07-02 19:45:40 -0300

    prova_sub_capel

--- back-end/src/controllers/users.jwt.js
+++ back-end/src/controllers/users.jwt.js
@@ -2,22 +2,14 @@ import prisma from '../database/client.js'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
-const controller = {}     // Objeto vazio
+const controller = {}
 
 controller.create = async function(req, res) {
   try {
-
-    // Criptografando a senha
     req.body.password = await bcrypt.hash(req.body.password, 12)
-
     await prisma.user.create({ data: req.body })
-
-    // HTTP 201: Created
     res.status(201).end()
-  }
-  catch(error) {
-    console.error(error)
-    // HTTP 500: Internal Server Error
+  } catch (error) {
     res.status(500).end()
   }
 }
@@ -25,143 +17,90 @@ controller.create = async function(req, res) {
 controller.retrieveAll = async function(req, res) {
   try {
     const result = await prisma.user.findMany()
-
-    // Deleta o campo "password", para não ser enviado ao front-end
-    for(let user of result) {
-      if(user.password) delete user.password
+    for (let user of result) {
+      if (user.password) delete user.password
     }
-
-    // HTTP 200: OK (implícito)
     res.send(result)
-  }
-  catch(error) {
-    console.error(error)
-    // HTTP 500: Internal Server Error
+  } catch (error) {
     res.status(500).end()
   }
 }
 
-controller.retrieveOne = async function (req, res) {
+controller.retrieveOne = async function(req, res) {
   try {
     const result = await prisma.user.findUnique({
-      where: { id: Number(req.params.id)}
+      where: { id: Number(req.params.id) }
     })
-
-    // Deleta o campo "password", para não ser enviado ao front-end
-    if(result.password) delete result.password
-
-    // Encontrou: retorna HTTP 200: OK (implícito)
-    if(result) res.send(result)
-    // Não encontrou: retorna HTTP 404: Not Found
+    if (result.password) delete result.password
+    if (result) res.send(result)
     else res.status(404).end()
-  }
-  catch(error) {
-    console.error(error)
-    // HTTP 500: Internal Server Error
+  } catch (error) {
     res.status(500).end()
   }
 }
 
-controller.update = async function (req, res) {
+controller.update = async function(req, res) {
   try {
-
-    // Criptografando o campo password caso o valor tenha sido passado
-    if(req.body.password) {
+    if (req.body.password) {
       req.body.password = await bcrypt.hash(req.body.password, 12)
     }
-
     const result = await prisma.user.update({
       where: { id: Number(req.params.id) },
       data: req.body
     })
-
-    // Encontrou e atualizou ~> HTTP 204: No Content
-    if(result) res.status(204).end()
-    // Não encontrou (e não atualizou) ~> HTTP 404: Not Found
+    if (result) res.status(204).end()
     else res.status(404).end()
-  }
-  catch(error) {
-    console.error(error)
-    // HTTP 500: Internal Server Error
+  } catch (error) {
     res.status(500).end()
   }
 }
 
-controller.delete = async function (req, res) {
+controller.delete = async function(req, res) {
   try {
     const result = await prisma.user.delete({
       where: { id: Number(req.params.id) }
     })
-
-    // Encontrou e excluiu ~> HTTP 204: No Content
-    if(result) res.status(204).end()
-    // Não encontrou (e não excluiu) ~> HTTP 404: Not Found
+    if (result) res.status(204).end()
     else res.status(404).end()
-  }
-  catch(error) {
-    console.error(error)
-    // HTTP 500: Internal Server Error
+  } catch (error) {
     res.status(500).end()
   }
 }
 
 controller.login = async function(req, res) {
-
   try {
-    // Busca o usuário pelo username
     const user = await prisma.user.findUnique({
       where: { username: req.body.username }
     })
-    // Se o usuário não for encontrado, retorna
-    // HTTP 401: Unauthorized
-    if(! user) return res.status(401).end()
-    // Usuário encontrado, vamos conferir a senha
+    if (!user) return res.status(401).end()
     const passwordMatches = await bcrypt.compare(req.body.password, user.password)
-    // Se a senha não confere ~> HTTP 401: Unauthorized
-    if(! passwordMatches) return res.status(401).end()
+    if (!passwordMatches) return res.status(401).end()
 
-    // Formamos o token de autenticação para enviar ao front-end
     const token = jwt.sign(
-      user,   // O token contém as informações do usuário logado
-      process.env.TOKEN_SECRET,   // Senha de criptografia do token
-      { expiresIn: '24h' }        // Prazo de validade do token
+      user,
+      process.env.TOKEN_SECRET,
+      { expiresIn: '24h' }
     )
 
-    // Forma o cookie para enviar ao front-end
     res.cookie(process.env.AUTH_COOKIE_NAME, token, {
-      httpOnly: true,   // O cookie ficará inacessível para JS no front-end
+      httpOnly: true,
       secure: true,
       sameSite: 'None',
       path: '/',
-      maxAge: 24 * 60 * 60 * 1000   // 24 horas
+      maxAge: 24 * 60 * 60 * 1000
     })
-
-    // Envia o token na resposta com código HTTP 200: OK (implícito)
-    //res.send({token})
-
-    // HTTP 204: No Content
     res.status(204).end()
-
-  }
-  catch(error) {
-    console.error(error)
-    // HTTP 500: Internal Server Error
+  } catch (error) {
     res.status(500).end()
   }
 }
 
 controller.me = function(req, res) {
-
-  // Se o usuário autenticado estiver salvo em req,
-  // retorna-o
-  if(req.authUser) res.send(req.authUser)
-
-  // Senão, retorna HTTP 401: Unauthorized
+  if (req.authUser) res.send(req.authUser)
   else res.status(401).end()
 }
 
 controller.logout = function(req, res) {
-  // Apaga o cookie que armazena o token de autorização
   res.clearCookie(process.env.AUTH_COOKIE_NAME)
   res.send(204).end()
 }
--- back-end/src/middleware/auth.jwt.js
+++ back-end/src/middleware/auth.jwt.js
@@ -1,77 +1,36 @@
 import jwt from 'jsonwebtoken'
 
 export default function(req, res, next) {
-
-  // As rotas que eventualmente não necessitarem
-  // de autenticação devem ser colocadas no
-  // objeto abaixo
   const bypassRoutes = [
     { url: '/users/login', method: 'POST' },
     { url: '/users', method: 'POST' }
   ]
 
-  // Verifica se a rota atual está nas exceções
-  // de bypassRoutes. Caso esteja, passa para o
-  // próximo middleware sem verificar a autenticação
-  for(let route of bypassRoutes) {
-    if(route.url === req.url && route.method === req.method) {
-      console.log(`Rota ${route.url}, método ${route.method} não autenticados por exceção`)
+  for (let route of bypassRoutes) {
+    if (route.url === req.url && route.method === req.method) {
       next()
       return
     }
   }
-  
-  // Para todas as demais rotas, é necessário que o token tenha
-  // sido enviado em um cookie ou no cabeçalho Authorization
 
-  let token = null
+  let token = req.cookies[process.env.AUTH_COOKIE_NAME]
 
-  console.log({ COOKIE: req.cookies[process.env.AUTH_COOKIE_NAME] })
-
-  // 1. PROCURA O TOKEN EM UM COOKIE
-  token = req.cookies[process.env.AUTH_COOKIE_NAME]
-
-  // 2. SE O TOKEN NÃO FOI ENCONTRADO NO COOKIE, PROCURA NO HEADER
-  // DE AUTORIZAÇÃO
-  if(! token) {
+  if (!token) {
     const authHeader = req.headers['authorization']
-
-    // O header não existe, o token não foi passado:
-    // HTTP 403: Forbidden
-    if(! authHeader) {
-      console.error('ERRO: não autenticado por falta de cookie ou cabeçalho de autorização')
+    if (!authHeader) {
       return res.status(403).end()
     }
-  
-    // O header Authorization é enviado como uma string
-    // Bearer: XXXX
-    // onde XXXX é o token. Portanto, para extrair o token,
-    // precisamos recortar a string no ponto onde há um espaço
-    // e pegar somente a a segunda parte
-    const [ , _token] = authHeader.split(' ')
-    
+
+    const [, _token] = authHeader.split(' ')
     token = _token
   }
 
-  // Valida o token
   jwt.verify(token, process.env.TOKEN_SECRET, (error, user) => {
-
-    // Token inválido ou expirado
-    // HTTP 403: Forbidden
-    if(error) {
-      console.error('ERRO: token inválido ou expirado')
+    if (error) {
       return res.status(403).end()
     }
 
-    /*
-      Se chegamos até aqui, o token está OK e temos as informações
-      do usuário logado no parâmetro 'user'. Vamos guardá-lo no 'req'
-      para futura utilização
-    */
     req.authUser = user
-    
-    // Continua para a rota normal
     next()
   })
-
 }
